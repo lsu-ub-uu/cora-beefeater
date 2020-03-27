bc894a163e345582171bb18ae3750923f0fb01fd
olov <olov@2abf716187cd>
2020-03-27 15:52:04 +0000

    javadoc updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,9 @@
+/**
+ * The beefeater module provides functionality to check users access to functions and data in a Cora
+ * based system.
+ * <p>
+ * The name beefeater is a tribute to the Beefeaters, the guardians of the Tower of London
+ */
 module se.uu.ub.cora.beefeater {
 	exports se.uu.ub.cora.beefeater;
 	exports se.uu.ub.cora.beefeater.authentication;
